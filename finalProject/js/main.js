92ece778d6cfbf401e51b6db59e683a750039e33
= <johnathanruiz@gmail.com>
2023-06-20 03:09:06 -0500

    add json

---
+++ finalProject/js/main.js
@@ -0,0 +1,4 @@
+fetch('https://fakestoreapi.com/products?limit=5')
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+let productList = document.insertById("#products");
