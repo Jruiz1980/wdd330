982f0e02a3112101e7b847cd81a27ca87297fa53
= <johnathanruiz@gmail.com>
2023-06-20 10:30:31 -0500

    add changes

--- finalProject/js/main.js
+++ finalProject/js/main.js
@@ -1,4 +1,29 @@
+let requestURL = "https://fakestoreapi.com/products";
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    const products = request.response;
+    populateHeader(products);
+    products(products);
+}
+
+function populateHeader(jsonObj) {
+    const myH1 = document.createElement('h1');
+    myH1.textContent = jsonObj['title'];
+    header.appendChild(myH1);
+  
+    const myPara = document.createElement('p');
+    myPara.textContent = jsonObj['description'] + ' //price: ' + jsonObj['price'];
+    header.appendChild(myPara);
+
+    const myImg = document.createElement('img')
+    myImg.setAttribute('src', jsonObj['image'])
+    header.appendChild(myImg)
+}
+
 fetch('https://fakestoreapi.com/products?limit=5')
             .then(res=>res.json())
             .then(json=>console.log(json))
-let productList = document.insertById("#products");
+let productList = document.insertById("#products")
