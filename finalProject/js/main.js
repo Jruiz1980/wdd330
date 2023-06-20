c8b20923261cf9f23b1414337f3dc82183ff1d6c
= <johnathanruiz@gmail.com>
2023-06-20 11:41:48 -0500

    add changes

--- finalProject/js/main.js
+++ finalProject/js/main.js
@@ -1,15 +1,5 @@
-let requestURL = "https://fakestoreapi.com/products";
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-    const products = request.response;
-    populateHeader(products);
-    products(products);
-}
 
-function populateHeader(jsonObj) {
+/*function populateHeader(jsonObj) {
     const myH1 = document.createElement('h1');
     myH1.textContent = jsonObj['title'];
     main.appendChild(myH1);
@@ -23,7 +13,71 @@ function populateHeader(jsonObj) {
     main.appendChild(myImg)
 }
 
-fetch('https://fakestoreapi.com/products?limit=5')
+/*fetch('https://fakestoreapi.com/products?limit=5')
             .then(res=>res.json())
             .then(json=>console.log(json))
 let productList = document.insertById("#products")
+
+
+let requestURL = "https://fakestoreapi.com/products";
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    const products = request.response;
+    populateHeader(products);
+    products(products);
+}*/
+function populateHeader(jsonObj) {
+    const myH1 = document.createElement('h1');
+    myH1.textContent = jsonObj['squadName'];
+    header.appendChild(myH1);
+
+    const myPara = document.createElement('p');
+    myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
+    header.appendChild(myPara);
+}
+function showHeroes(jsonObj) {
+  const heroes = jsonObj["members"];
+
+  for (var i = 0; i < heroes.length; i++) {
+    const myArticle = document.createElement("article");
+    const myH2 = document.createElement("h2");
+    const myPara1 = document.createElement("p");
+    const myPara2 = document.createElement("p");
+    const myPara3 = document.createElement("p");
+    const myList = document.createElement("ul");
+
+    myH2.textContent = heroes[i].name;
+    myPara1.textContent = "Secret identity: " + heroes[i].secretIdentity;
+    myPara2.textContent = "Age: " + heroes[i].age;
+    myPara3.textContent = "Superpowers:";
+
+    const superPowers = heroes[i].powers;
+    for (var j = 0; j < superPowers.length; j++) {
+      const listItem = document.createElement("li");
+      listItem.textContent = superPowers[j];
+      myList.appendChild(listItem);
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
+}
+  
+const requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+    const superHeroes = request.response;
+    populateHeader(superHeroes);
+    showHeroes(superHeroes);
+}
