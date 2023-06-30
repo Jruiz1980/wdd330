28b56d677e84890c329f5be51d0d6f78f290a4eb
= <johnathanruiz@gmail.com>
2023-06-30 04:21:55 -0500

    add changes

--- finalProject/js/main.js
+++ finalProject/js/main.js
@@ -91,7 +91,11 @@ fetch('https://fakestoreapi.com/products')
   .then(data => {
     let prods = document.getElementById('products');
     for (prod of data) {
-      prods.innerHTML += `<img src="${prod.image}" width='100px' height='150px' id="prodImg" alt="">`;
+      prods.innerHTML += `<section>
+                            <img src="${prod.image}" width='100px' height='150px' id="prodImg" alt="">
+                            <p>${prod.title}</p>
+                            <p>$${prod.price.toFixed(2)}</p>
+                          </section>`;
     }
   })
   .catch(error => console.error(error));
