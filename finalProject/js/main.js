94329135997cc3a6f3e7cd273004e815201cf197
= <johnathanruiz@gmail.com>
2023-07-11 23:15:56 -0500

    add changes

--- finalProject/js/main.js
+++ finalProject/js/main.js
@@ -5,7 +5,7 @@ fetch('https://fakestoreapi.com/products/category/electronics')
     let prods = document.getElementById('products');
     for (prod of data) {
       prods.innerHTML += `<section class="${prod.category}">
-                            <img src="${prod.image}" width='100px' height='150px' id="prodImg" alt="">
+                            <img src="${prod.image}" width='150px' height='150px' id="prodImg" alt="">
                             <p>${prod.title}</p>
                             <p>$${prod.price.toFixed(2)}</p> 
                           </section>`;
