c9c8c26c609f361cd2e255887ce1b2815452dfa6
= <johnathanruiz@gmail.com>
2023-07-15 20:32:55 -0500

    add changes

--- finalProject/js/cart.js
+++ finalProject/js/cart.js
@@ -24,19 +24,41 @@
         let carritoContent = document.createElement("div")
         carritoContent.className = "modal-content"
         carritoContent.innerHTML = `
-        <img src="${product.image}">
-        <h3>${product.title}</h3>
-        <p class="price">$ ${product.price.toFixed(2)}</p>
+            <img src="${product.image}">
+            <h3>${product.title}</h3>
+            <p>$${product.price.toFixed(2)}</p>
+            <p> Quantity: ${product.quantity}</p>
+            <p> Total: $${product.quantity * product.price} 
         `;
 
         modalContainer.append(carritoContent)
+
+        let remove = document.createElement("span");
+        remove.innerText = "\uD83D\uDDD1\uFE0F";
+        remove.className = "remove-product";
+        carritoContent.append(remove);
+
+        remove.addEventListener("click", removeProduct)
     });
 
-    const total = carrito.reduce((acc, el) => acc + el.price, 0);
+    const total = carrito.reduce((acc, el) => acc + el.price * el.quantity, 0);
 
     const totalBuying = document.createElement("div")
     totalBuying.className = "total-content"
-    totalBuying.innerHTML = `TOTAL:$ ${total}`;
+    totalBuying.innerHTML = `TOTAL: $${total.toFixed(2)}`;
     modalContainer.append(totalBuying);
 }
 shopping.addEventListener("click", shopCart);
+
+const removeProduct = () => {
+    const findId = carrito.find((element) => element.id);
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== findId;
+    });
+    shopCart();
+};
+
+const counterItem = () => {
+    cantidadCarrito.style.display = "block";
+    cantidadCarrito.innerText = carrito.length;
+    }
--- finalProject/js/shopping-cart.js
+++ finalProject/js/shopping-cart.js
@@ -6,94 +6,22 @@ const modalContainer = document.getElementById("modal-container");
 const showAlert = document.getElementById("showAlert");
 const cantidadCarrito = document.getElementById("cantidadCarrito");
 
-/*let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-
-const getProducts = async () => {
-    const response = await fetch('https://fakestoreapi.com/products');
-    const data = await response.json();
-    console.log(data);
-};
-
-getProducts();
-
-data.forEach((prod)=> {
-    let content = document.createElement("div");
-    content.className = "card";
-    content.innerHTML =`
-    <section class="${prod.category}">
-        <img src="${prod.image}" width='150px' height='150px' id="prodImg" alt="">
-        <p>${prod.title}</p>
-        <p>$${prod.price.toFixed(2)}</p> 
-    </section>`;
-
-    let comprar = document.createElement("button");
-    comprar.innerText = "comprar";
-    comprar.className = "comprar";
-
-    content.append(comprar);
-
-    comprar.addEventListener("click", () => {
-        const repeat = carrito.some((repeatProduct) => repeatProduct.Id === product.id);
-
-        if(repeat) {
-            carrito.map((prod) => {
-                if (prod.id === product.id) {
-                    prod.cantidad++;
-                }
-            });
-        } 
-        else{
-            carrito.push({
-                id: product.id,
-                image: product.image,
-                name: product.name,
-                price: product.price,
-                cantidad: product.cantidad,
-            });
-            console.log(carrito);
-            console.log(carrito.length);
-            carritoCounter();
-            savelocal();
-        }
-    });
-});
-
-shopping.addEventListener("click", () => {
-    const modalHeader = document.createElement("div");
-    modalHeader.className = "modal-header"
-    modalHeader.innerHTML = `
-    <h1 class="modal-header-title">Carrito.</h1>
-    `;
-    modalContainer.append(modal-header);
-
-     const modalButton = document.createElement("h1")
-     modalButton.innerText = "x";
-     modalButton.className = "modal-header-button";
-
-     modalHeader.append(modalButton);
-
-     let shoppingContent = document.createElement("div");
-     carrito.className = "modal-content";
-     carrito.innerHTML = `
-     
-     `
-})
-let carrito = [];
-async function loadJSONS(){
-    carrito.push(await fetch('https://fakestoreapi.com/products').then(res => res.json()));
-}
-loadJSONS();
-console.log(carrito);
-
-*/
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 async function getProducts() {
     const response = await fetch('https://fakestoreapi.com/products');
     const data = await response.json();
+    const modifiedData = data.map(product => {
+        return {
+          ...product,
+           quantity:1
+            };
+        });
 
-    data.forEach((product) => {
+    console.log(modifiedData);
+
+    modifiedData.forEach((product) => {
         let content = document.createElement("div");
         content.className = "card";
         content.innerHTML = `
@@ -101,28 +29,38 @@ async function getProducts() {
             <h3>${product.title}</h3>
             <p class="price">$ ${product.price.toFixed(2)}</p>
             `;
-        console.log(data);
+        console.log(product);
         
-
         shopContent.append(content);
 
-        let comprar = document.createElement("button")
-        comprar.innerText = "comprar";
-        comprar.className = "comprar";
+        let buy = document.createElement("button")
+        buy.innerText = "BUY";
+        buy.className = "buy";
 
-        content.append(comprar);
+        content.append(buy);
 
-        comprar.addEventListener("click", () => {
+        buy.addEventListener("click", () => {
+
+            const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+            if(repeat) {
+                carrito.map((prod) => {
+                    if (prod.id === product.id) {
+                        prod.quantity++;
+                    }
+                });
+            } else {
+
             carrito.push({
                 id: product.id,
-                    image: product.image,
-                    title: product.title,
-                    price: product.price,
-                    cantidad: product.cantidad,
-            });
+                image: product.image,
+                title: product.title,
+                price: product.price,
+                quantity: product.quantity,
+                });
+            }
+        counterItem();
         });
     });
-};
-
+}
 getProducts();
     
