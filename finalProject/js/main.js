c1a5c8e389c5f0c56e107ea1e56e9efd9e761eb5
= <johnathanruiz@gmail.com>
2023-07-18 09:45:43 -0500

    add changes

--- finalProject/js/main.js
+++ finalProject/js/main.js
@@ -1,5 +1,5 @@
 
-fetch('https://fakestoreapi.com/products')
+/*fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
   .then(data => {
     let prods = document.getElementById('products');
@@ -13,4 +13,70 @@ fetch('https://fakestoreapi.com/products')
     }
   })
 
-  .catch(error => console.error(error));
+  .catch(error => console.error(error));*/
+  
+
+const shopContent = document.getElementById("shopContent");
+const shopping = document.getElementById("shopping");
+const modalContainer = document.getElementById("modal-container");
+const showAlert = document.getElementById("showAlert");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+async function getProducts() {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    const modifiedData = data.map(product => {
+        return {
+          ...product,
+           quantity:1
+            };
+        });
+
+    console.log(carrito);
+
+    modifiedData.forEach((product) => {
+        let content = document.createElement("div");
+        content.className = "card";
+        content.innerHTML = `
+            <img src="${product.image}">
+            <h3>${product.title}</h3>
+            <p class="price">$ ${product.price.toFixed(2)}</p>
+            `;
+        console.log(product);
+        
+        shopContent.append(content);
+
+        let buy = document.createElement("button")
+        buy.innerText = "BUY";
+        buy.className = "buy";
+
+        content.append(buy);
+
+        buy.addEventListener("click", () => {
+
+            const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+            if(repeat) {
+                carrito.map((prod) => {
+                    if (prod.id === product.id) {
+                        prod.quantity++;
+                    }
+                });
+            } else {
+
+            carrito.push({
+                id: product.id,
+                image: product.image,
+                title: product.title,
+                price: product.price,
+                quantity: product.quantity,
+                });
+            }
+        counterItem();
+        });
+    });
+}
+getProducts();
+    
